832b59dd8f1956de5c7c72d6bc08b127d00e5b5c
Luca Abeni <luca.abeni@unitn.it>
2014-12-11 10:18:51 +0100

    Get a fixed version of rt-app

--- migration_bug/get_rt-app.sh
+++ migration_bug/get_rt-app.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/scheduler-tools/rt-app.git
 cd rt-app
+git am ../0001-Allow-to-synchronize-all-the-tasks-at-start.patch
 ./autogen.sh
 ./configure --with-deadline --with-json
 make
